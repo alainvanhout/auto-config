97d04ecb822ec1c2eaba6e82ea6b3ed7912a110b
Alain Van Hout <alainvanhout@gmail.com>
2016-08-29 19:48:19 +0200

    initial commit

---
+++ spring-auto-config/src/main/java/autoconfig/EnableAutoConfig.java
@@ -0,0 +1,12 @@
+package autoconfig;
+
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Import(value = Config.class)
+public @interface EnableAutoConfig {
+}
